d5a43435f2467c6062ba06ce77810a5be5555bec
pbaudet <pbaudet.enseirb@gmail.com>
2026-06-23 08:25:06 +0200

    github cicd actions

--- libraries/python/jordan_py/jordan_py/test/test_jordan.py
+++ libraries/python/jordan_py/jordan_py/test/test_jordan.py
@@ -5,21 +5,22 @@ import unittest
 class TestSimple(unittest.TestCase):
 
     def test_action_builder(self):
-        actions = jordan \
-            .with_action('shoot') \
-            .with_parameter('player_name', jordan.PARAMETER_TYPE_STRING, default_value='Jordan') \
-            .with_parameter('points', jordan.PARAMETER_TYPE_INT) \
-            .build()
+        actions = (jordan
+                   .with_action('shoot')
+                   .with_parameter('player_name', jordan.PARAMETER_TYPE_STRING, default_value='Jordan')
+                   .with_parameter('points', jordan.PARAMETER_TYPE_INT)
+                   .build())
         self.assertIsNotNone(actions)
-        self.assertEquals(len(actions), 1)
-        self.assertEquals(actions[0]['actionName'], 'shoot')
-        self.assertEquals(len(actions[0]['actionName']['parameters']), 2)
-        self.assertEquals(actions[0]['actionName']['parameters'][0]['name'], 'player_name')
-        self.assertEquals(actions[0]['actionName']['parameters'][0]['type'], 'string')
-        self.assertEquals(actions[0]['actionName']['parameters'][0].get('defaultValue'), 'Jordan')
-        self.assertEquals(actions[0]['actionName']['parameters'][1]['name'], 'points')
-        self.assertEquals(actions[0]['actionName']['parameters'][1]['type'], 'int')
-        self.assertIsNone(KeyError, actions[0]['actionName']['parameters'][0].get('defaultValue'))
+        self.assertEqual(len(actions), 1)
+        self.assertEqual(actions[0]['actionName'], 'shoot')
+        params = actions[0]['parameters']
+        self.assertEqual(len(params), 2)
+        self.assertEqual(params[0]['name'], 'player_name')
+        self.assertEqual(params[0]['type'], 'string')
+        self.assertEqual(params[0].get('defaultValue'), 'Jordan')
+        self.assertEqual(params[1]['name'], 'points')
+        self.assertEqual(params[1]['type'], 'int')
+        self.assertIsNone(params[1].get('defaultValue'))
 
 
 if __name__ == '__main__':
